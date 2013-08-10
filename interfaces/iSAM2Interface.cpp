ae0797799ec3ee1be40474bd438130602e8cd81d
Kourosh Sartipi <kourosh.sartipi@gmail.com>
2013-08-10 20:09:24 +0430

    Added iSAM2 support and done basic debuggings

--- interfaces/iSAM2Interface.cpp
+++ interfaces/iSAM2Interface.cpp
@@ -19,7 +19,14 @@
  */
 
 #include <interfaces/iSAM2Interface.h>
+
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/nonlinear/Symbol.h>
+#include <fcntl.h>
+
 
 
 namespace KSRobot
@@ -27,8 +34,18 @@ namespace KSRobot
 namespace interfaces
 {
 
-iSAM2Interface::iSAM2Interface() : common::SLAMInterface(), mLastestCycle(0)
+static inline std::string KeyToString(const gtsam::Key& k)
 {
+    return gtsam::Symbol(k);
+}
+
+iSAM2Interface::iSAM2Interface() : common::SLAMInterface(), mLastestCycle(0), mAddFeatures(false)
+{
+    // Noise model is x, y, z, roll, pitch, yaw (in meters and radians, respectively)
+    mPriorNoise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector_(6, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+    mOdometryNoise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector_(6, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+    mLoopClosureNoise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector_(6, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+    
     SetInterfaceName("iSAM2Interface");
     Initialize();
 }
@@ -41,69 +58,191 @@ void iSAM2Interface::Initialize()
 {
     //TODO: Change these by model
     mISAM2 = new gtsam::ISAM2();
-    mPriorNoise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector_(1,0,0,1));
-    mOdometryNoise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector_(1,0,0,1));
+    mKeysCollection.clear();
+    mNewFactors.resize(0);
+    mNumFeatures = 0;
+    mNumNodes = 0;
 }
-
 
 void iSAM2Interface::ReadSettings(common::ProgramOptions::Ptr po)
 {
-    //TODO:
+    common::SLAMInterface::ReadSettings(po);
+    
+    mPriorNoise = gtsam::noiseModel::Diagonal::Sigmas(ReadSigmas(po->StartNode("PriorNoise")));
+    mOdometryNoise = gtsam::noiseModel::Diagonal::Sigmas(ReadSigmas(po->StartNode("OdometryNoise")));
+    mLoopClosureNoise = gtsam::noiseModel::Diagonal::Sigmas(ReadSigmas(po->StartNode("LoopClosureNoise")));
+    
+    mAddFeatures = po->GetBool("UseFeatures", true);
     
     Initialize();
 }
 
+gtsam::Vector iSAM2Interface::ReadSigmas(common::ProgramOptions::Ptr po)
+{
+    double x, y, z, roll, pitch, yaw;
+    x = po->GetDouble("X", 0.1);
+    y = po->GetDouble("Y", 0.1);
+    z = po->GetDouble("Z", 0.1);
+    roll = po->GetDouble("Roll", 0.1);
+    pitch = po->GetDouble("Pitch", 0.1);
+    yaw = po->GetDouble("Yaw", 0.1);
+    
+    return gtsam::Vector_(6, x, y , z, roll, pitch, yaw);
+}
+
+gtsam::Pose3 Convert(const Eigen::Isometry3f& iso)
+{
+    gtsam::Matrix3 mat = iso.linear().cast<double>();
+    Eigen::Vector3d trans = iso.translation().cast<double>();
+    return gtsam::Pose3(mat, gtsam::Point3(trans));
+}
+
 void iSAM2Interface::AddKeyframe(const common::VisualKeyframe::Ptr kf)
 {
-    //TODO: IMPLEMENT
-    bool ref_frame_found = (mSeenCycles.find(kf->ReferenceCycle) != mSeenCycles.end());
+//     Debug("(iSAM2Interface::AddKeyframe) New keyframe. Ref cycle = %u, Curr cycle = %u\n", kf->ReferenceCycle, kf->CurrentCycle);
+    //Add reference frame.
+    mSeenCycles.insert(kf->ReferenceCycle);
+    mSeenCycles.insert(kf->CurrentCycle);
+
+//     Debug("Cycle %d\n", GetCycle());
     
-    if( !ref_frame_found && mLastestCycle == 0 )
+    Eigen::Matrix3d rot_mat = kf->RelativeMotion.linear().cast<double>();
+    gtsam::Rot3 rot(rot_mat);
+    gtsam::Point3 point(kf->RelativeMotion.translation().cast<double>());
+    gtsam::Pose3 pose(rot, point);
+    
+    bool ref_found, curr_found;
+    gtsam::Key ref_key = GetOrCreateNodeKey(kf->ReferenceCycle, &ref_found);
+    gtsam::Key curr_key = GetOrCreateNodeKey(kf->CurrentCycle, &curr_found);
+    
+    if( mNumNodes - 2 == 0 ) // this is becuase we create 2 nodes just before this
     {
-        // this is the first cucle
-        
+        mNewFactors.add(gtsam::PriorFactor<gtsam::Pose3>(ref_key, gtsam::Pose3::identity(), mPriorNoise));
+        mValues.insert(ref_key, gtsam::Pose3::identity());
     }
-    else if( !ref_frame_found )
+    
+    mNewFactors.add(gtsam::BetweenFactor<gtsam::Pose3>(ref_key, 
+                                                       curr_key,
+                                                       pose, mOdometryNoise));//between factor 
+    
+    //TODO: IS THIS CORRECT?
+    mValues.insert(curr_key, Convert(kf->GlobalPose));
+    
+//     Debug("ref_key = %s, curr_key = %s\n", KeyToString(ref_key).c_str(), KeyToString(curr_key).c_str());
+//     Debug("Between factors added, ref_found = %s, curr_found = %s\n", 
+//           ref_found ? "true" : "false",
+//           curr_found ? "true" : "false");
+    
+    if( mAddFeatures )
     {
-        // This means this is not the first cycle, but reference frame was not found.
-        // What this means is that the reference frame was changed by the VO algorithm
-        // internally but the change was not registered as keyframe e.g. when FOVIS fails
-        // to calculate motion from the reference frame but can calculate it using previous frame.
-        
-        //NOTE: in this case, since there are no nodes for the ref_frame, we should ignore unfortunately.
-        Debug("Reference frame %d was not found.\n", kf->ReferenceCycle);
-        return;
-    }
-    else
-    {
-        assert(ref_frame_found == true);
-        // This means it is not the first cycle, and reference frame was found. In this case
-        // we should add a node to the graph for this frame (the reference frame is already in the graph)
-        // and we should add feature matches between this frame and reference frame.
+        for(size_t i = 0; i < kf->MatchedPairs.size(); i++)
+            GetOrCreateFeatureKey(kf->ReferenceCycle, kf->MatchedPairs[i].ReferenceIndex);
+        //TODO: Complete this.
     }
 }
 
 void iSAM2Interface::AddLoopClosure(const common::LoopDetector::LoopClosure& lc)
 {
-    //TODO:
-    // NOTE: For loop closure, both frames should have been visited and exist inside the graph.
+    //NOTE: For loop closure, both frames should have been visited and exist inside the graph.
     if( mSeenCycles.find(lc.Cycle1) == mSeenCycles.end() || mSeenCycles.find(lc.Cycle2) == mSeenCycles.end() )
     {
         // THIS IS BAD
-        if( lc.Cycle1 > mLastestCycle || lc.Cycle2 > mLastestCycle )
-        {
-            // This means we have cycles not registered to the graph.
-            //TODO: We should add it to the queue again, but it could cause infinite loop.
-            Error("Unknown loop closure cycles (%lu, %lu) were passed. Latest Cycle = %lu.\n", lc.Cycle1, lc.Cycle2, mLastestCycle);
-        }
-        return; // in any case, do no furthur processing.
+        Error("(iSAM2Interface::AddLoopClosure) Unknown loop closure cycles (%lu, %lu) were passed. Latest Cycle = %lu.\n", 
+                  lc.Cycle1, lc.Cycle2, mLastestCycle);
+        return;
     }
-    //TODO: IMPLEMENT
+    
+    gtsam::Key key1, key2;
+    bool key1_created, key2_created;
+    
+    key1 = GetOrCreateNodeKey(lc.Cycle1, &key1_created);
+    key2 = GetOrCreateNodeKey(lc.Cycle2, &key2_created);
+    
+    assert( (!key1_created) && (!key2_created) );// none of the keys should be created, they should already exist
+    
+    gtsam::Matrix3 mat = lc.Transform.linear().cast<double>();
+    gtsam::Rot3 rot(mat);
+    gtsam::Point3 trans(lc.Transform.translation().cast<double>());
+    gtsam::Pose3 pose(rot, trans);
+    
+    mNewFactors.add(gtsam::BetweenFactor<gtsam::Pose3>(key1, key2, pose, mLoopClosureNoise));
 }
 
 void iSAM2Interface::Update()
 {
-    //TODO: Implement;
+//     Debug("New factors:\n");
+//     mNewFactors.print();
+//     Debug("New values:\n");
+//     mValues.print();
+//     Debug("before update\n");
+    mISAM2->update(mNewFactors, mValues);
+//     Debug("after update\n\n");
+    //In the final act, clear new factors
+    mNewFactors.resize(0);
+    mValues.clear();
+}
+
+gtsam::Key iSAM2Interface::CreateFeatureKey()
+{
+    return gtsam::Symbol('l', mNumFeatures++);
+}
+
+gtsam::Key iSAM2Interface::CreateNodeKey()
+{
+    return gtsam::Symbol('x', mNumNodes++);
+}
+
+gtsam::Key iSAM2Interface::GetOrCreateNodeKey(std::size_t node_cycle, bool* was_created)
+{
+    bool created;
+    
+    gtsam::Key key;
+    IndexToNodeMap::iterator iter = mNodesCollection.find(node_cycle);
+    if( iter != mNodesCollection.end() )
+    {
+        created = false;
+        key = iter->second;
+    }
+    else
+    {
+        created = true;
+        key = CreateNodeKey();
+        mNodesCollection[node_cycle] = key;
+    }
+    
+    if( was_created )
+        *was_created = created;
+    
+    return key;
+}
+
+gtsam::Key iSAM2Interface::GetOrCreateFeatureKey(std::size_t node_cycle, int feature, bool* was_created)
+{
+    bool created;
+    gtsam::Key key;
+    
+    if( mKeysCollection.find(node_cycle) == mKeysCollection.end() )
+        mKeysCollection[node_cycle] = IndexToKeyMap();
+    
+    IndexToKeyMap& i2k = mKeysCollection[node_cycle];
+    IndexToKeyMap::iterator iter = i2k.find(feature);
+    
+    if( iter == i2k.end() )
+    {
+        created = true;
+        key = CreateFeatureKey();
+        i2k[feature] = key;
+    }
+    else
+    {
+        created = false;
+        key = iter->second;
+    }
+    
+    if( was_created )
+        *was_created = created;
+    
+    return key;
 }
 
 
--- interfaces/iSAM2Interface.h
+++ interfaces/iSAM2Interface.h
@@ -25,6 +25,7 @@
 #include <gtsam/nonlinear/ISAM2.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <set>
+#include <map>
 
 namespace KSRobot
 {
@@ -34,23 +35,52 @@ namespace interfaces
 class iSAM2Interface : public common::SLAMInterface
 {
 public:
+    typedef iSAM2Interface                              this_type;
+    typedef boost::shared_ptr<this_type>                Ptr;
+    typedef boost::shared_ptr<const this_type>          ConstPtr;
+    
     iSAM2Interface();
     virtual ~iSAM2Interface();
     
     virtual void Initialize();
     virtual void ReadSettings(common::ProgramOptions::Ptr po);
     
+    void            Print() { mISAM2->print(); }
+    
 protected:
     virtual void                AddKeyframe(const common::VisualKeyframe::Ptr kf);
     virtual void                AddLoopClosure(const common::LoopDetector::LoopClosure& lc);
     virtual void                Update();
+    
+    static gtsam::Vector        ReadSigmas(common::ProgramOptions::Ptr po);
+    
+    gtsam::Key                  CreateNodeKey();
+    gtsam::Key                  CreateFeatureKey();
+    gtsam::Key                  GetOrCreateNodeKey(size_t node_cycle, bool* was_created = NULL);
+    gtsam::Key                  GetOrCreateFeatureKey(size_t node_cycle, int feature, bool* was_created = NULL);
 private:
     gtsam::ISAM2*                               mISAM2;
-    gtsam::NonlinearFactorGraph                 mGraph;
+    gtsam::NonlinearFactorGraph                 mNewFactors;
+    gtsam::Values                               mValues;
+    
     gtsam::noiseModel::Diagonal::shared_ptr     mPriorNoise;
     gtsam::noiseModel::Diagonal::shared_ptr     mOdometryNoise;
+    gtsam::noiseModel::Diagonal::shared_ptr     mLoopClosureNoise;
+    
     std::set<size_t>                            mSeenCycles;
     size_t                                      mLastestCycle;
+    
+    typedef std::map<int, gtsam::Key>           IndexToKeyMap;
+    typedef std::map<size_t, IndexToKeyMap>     IndexToKeyCollection;
+    typedef std::map<size_t, gtsam::Key>        IndexToNodeMap;
+    
+    IndexToKeyCollection                        mKeysCollection;
+    IndexToNodeMap                              mNodesCollection;
+    
+    size_t                                      mNumFeatures; // this is to keep track of feature numbers.
+    size_t                                      mNumNodes; // this is actually number of keyframes received.
+    
+    bool                                        mAddFeatures;
 };
 
 };
